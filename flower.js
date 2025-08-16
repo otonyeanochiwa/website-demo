a395b37383f0b3c7a4fac0c777d1fa4a983bc5ea
otonyeanochiwa <anochiwaotonye@gmail.com>
2025-08-16 14:36:24 +0100

    Add files via upload

---
+++ flower.js
@@ -0,0 +1,72 @@
+//     var but_one = document.getElementById("first_but")
+// var but_two = document.getElementById("second_but")
+// var but_three = document.getElementById("third_but")
+    
+// var div1 = document.getElementById("left") 
+// var div2 = document.getElementById("middle")
+// var div3 = document.getElementById("right")
+
+// var gen = document.getElementById("gen")
+  
+
+// div1.onmouseover = function() {
+//     // alert("i am a boy")
+    
+//     // div1.style.width ="653px"
+//     div2.style.opacity ="30%"
+//     div3.style.opacity ="30%"
+//      div1.style.opacity = "100"
+    
+//  }
+
+
+
+// div2.onmouseover = function() {
+//     div2.style.opacity = "100"
+//     div1.style.opacity ="30%"
+//     div3.style.opacity ="30%"
+
+    
+//  }
+
+
+
+// div3.onmouseover = function() {
+//     // alert("i am a boy")
+
+//     // div3.style.width ="653px"
+//     div2.style.opacity ="30%"
+//      div3.style.opacity = "100"
+//     div1.style.opacity ="30%"
+
+    
+//  }
+
+//  gen.onclick = function (params) {
+//     div3.style.opacity = "100"
+//     div2.style.opacity = "100"
+//     div1.style.opacity = "100"
+//  }
+// // div3.style.display ="block"
+
+ 
+
+
+
+
+// var one = document.getElementById("one")
+// var two = document.getElementById("two")
+// var three = document.getElementById("three")
+// var four = document.getElementById("four")
+// var display = document.getElementById("display")
+
+
+
+// // one.onclick = function names(params) {
+// //     alert("i am a boy")
+// // }
+
+// one.onmouseover = function (params) {
+//     display.innerHTML = "i am a boy"
+  
+// }
